ac557b58218412e4b07b0a5f23a5e6292fa0d8b7
NebulousCalm <zjrichman@outlook.com>
2024-06-14 17:05:18 -0400

    did dark/light theme toggles + global css updates + temp demo file

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 ```shell
 pip install flask
 ```
+
+# Notes while developing
+
+Light/Dark mode toggle has a codepen demo here: [link](https://codepen.io/Z4CHr/pen/ZENymPJ)
--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello World!"
+
+
+@app.route('/temp/demo')
+def temp_demo():
+    return render_template('demo.temp.html')
 
 
 if __name__ == "__main__":
